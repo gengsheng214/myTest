3610b1804a64ee5a921b54e7959918f28aab790b
gengsheng <807811247@qq.com>
2026-03-22 20:05:41 +0800

    测试codereview

---
+++ swiftcv-web/src/main/java/com/gs/swiftcv/web/controller/kitchen/CodeReviewController.java
@@ -0,0 +1,33 @@
+package com.gs.swiftcv.web.controller.kitchen;
+
+import com.gs.swiftcv.service.model.KitchenCategoryVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author : gengsheng
+ * @Description:
+ * @Date: 2026/3/22 19:54
+ */
+@RestController
+@RequestMapping("codeReview")
+public class CodeReviewController {
+
+
+    @GetMapping("test")
+    public void test() {
+        KitchenCategoryVO kitchenCategoryVO = null;
+        String description = kitchenCategoryVO.getDescription();
+        List<String> list = new ArrayList<>();
+        list.add(description);
+        while (true) {
+            for (String s : list) {
+                System.out.println(s);
+            }
+        }
+    }
+}
